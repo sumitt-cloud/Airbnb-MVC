92a700df113498f6c29c2127f788a4586cda773e
sumitt-cloud <sumit.leonova1615@gmail.com>
2026-03-27 13:41:03 +0530

    adding context

--- routes/hostrouter.js
+++ routes/hostrouter.js
@@ -2,11 +2,12 @@ const express = require("express");
 
 const hostrouter = express.Router();
 
-const hostcontroller =  require('../controllers/storecontroller')
+const hostcontroller =  require('../controllers/hostcontroller')
 
 hostrouter.get("/add-home",  hostcontroller.getaddhome)
 
 hostrouter.post("/add-home", hostcontroller.postaddhome)
-hostrouter.get("/host-home-list", hostcontroller.gethosthome)
+
+hostrouter.get("/host-home-list", hostcontroller.gethosthomes)
 
 module.exports = hostrouter;
